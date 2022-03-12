0cc7a06dd15e6391923479f3e1b7370bc7c3087d
Daniel Brady <brady@prodperfect.com>
2022-03-12 18:24:24 +0000

    (deleteme) meaningless change

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /*
  * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
+  * https://jestjs.io/docs/en/configuration.html
  */
 
 module.exports = {
