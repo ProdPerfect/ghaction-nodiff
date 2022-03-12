b7a46bc50af994a71620056fdbbeb2e15f6cc579
Daniel Brady <brady@prodperfect.com>
2022-03-12 15:28:24 +0000

    getting closer

--- dist/index.js
+++ dist/index.js
@@ -57,8 +57,8 @@ function parseInputs() {
 }
 
 async function meaninglessDiff(filesToJudge, baseRef) {
-  var meaningfulDiffCmd = `/usr/bin/git diff --ignore-space-change --ignore-blank-lines --numstat origin/${baseRef} HEAD -- ${filesToJudge} | awk '{print \\$3}'`;
-  var meaninglessDiffCmd = `/usr/bin/comm -23 <(/usr/bin/git diff --name-only origin/${baseRef} HEAD -- ${filesToJudge}) <(${meaningfulDiffCmd})`;
+  var meaningfulDiffCmd = `git diff --ignore-space-change --ignore-blank-lines --numstat origin/${baseRef} HEAD -- ${filesToJudge} | awk '{print \\$3}'`;
+  var meaninglessDiffCmd = `comm -23 <(git diff --name-only origin/${baseRef} HEAD -- ${filesToJudge}) <(${meaningfulDiffCmd})`;
   var stdout = '';
   var stderr = '';
   try {
