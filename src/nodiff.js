3e63f3513bd3aa47377ea1621fc9f717cb42822c
Daniel Brady <brady@prodperfect.com>
2022-03-12 15:39:08 +0000

    mcduggion

--- src/nodiff.js
+++ src/nodiff.js
@@ -48,7 +48,7 @@ async function meaninglessDiff(filesToJudge, baseRef) {
       '/bin/bash',
       [
         '-c',
-        `comm -23 <(cat index.js) <(cat index.js)` // `"${meaninglessDiffCmd}"`
+        `comm -23 <(git diff --name-only origin/${baseRef} HEAD -- ${filesToJudge}) <(git diff --name-only origin/${baseRef} HEAD -- ${filesToJudge})` // `"${meaninglessDiffCmd}"`
       ],
       null,
       {
