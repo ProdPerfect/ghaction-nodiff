5038f5ee779807cc431489430f56c138d0a14b45
Daniel Brady <brady@prodperfect.com>
2022-03-16 14:58:45 +0000

    delete placeholder test for main action: no suitable unit tests

--- tests/nodiff.test.js
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@jest/globals';
-// import nodiff from '../src/nodiff';
-
-// TODO Write tests that test the thing.
-test('does the thing', function() {
-  expect(true).toBeTruthy();
-});
